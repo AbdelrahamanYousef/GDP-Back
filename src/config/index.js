d4418c0d94ce025d7d8459dd941acb9094f1ebf8
CowCat <114646151+CowCat271@users.noreply.github.com>
2026-02-03 21:43:24 +0200

    Update database URL to use environment variable

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const config = {
     port :Number(process.env.PORT) |3000,
     env: process.env.NODE_ENV || 'development',
-    dbUrl :"mongodb+srv://myDb:12345678abdo@cluster0.r13zie2.mongodb.net/"
+    dbURI : process.env.DVURI
 }
 
 module.exports = config;
