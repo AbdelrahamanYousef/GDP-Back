4905079800876d2dddca16bc31c96567d0fd51c4
Hani Samir <haneyselsaid2004@gmail.com>
2026-02-04 02:37:50 +0200

    restructure

--- src/config/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config()
-
-const config = {
-    port :Number(process.env.PORT) |3000,
-    env: process.env.NODE_ENV || 'development',
-    dbURI : process.env.DVURI
-}
-
-module.exports = config;
